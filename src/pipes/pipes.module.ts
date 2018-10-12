8212f5f369c02ed5751d821447f39f0e6f3cac02
Elvis Furcic <furcicelvis@gmail.com>
2018-10-12 21:41:06 +0200

    Added worktree

--- src/pipes/pipes.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { NumberFormatterPipe } from './number-formatter/number-formatter';
-@NgModule({
-	declarations: [NumberFormatterPipe],
-	imports: [],
-	exports: [NumberFormatterPipe]
-})
-export class PipesModule {}
